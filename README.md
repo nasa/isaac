ab81b321078c45c0ee2841f412375963d3e388d9
Marina Moreira <67443181+marinagmoreira@users.noreply.github.com>
2021-12-01 12:44:27 -0800

    fixing CI actions (#1)
    * running python files through linter
    
    * using the correct submodule name
    
    * removed slash
    
    * adding correct tag
    
    * fixing build for bionic
    
    * linking readme and adding more discription

--- README.md
+++ README.md
@@ -2,14 +2,26 @@
 
 ### About
 
+The Integrated System for Autonomous and Adaptive Caretaking project is developing technology for combining robots inside a spacecraft with vehicle infrastructure subsystems, to form an integrated autonomous system.
+The system validates its technology through demonstrations using the Astrobee free-flier robots, existing robots onboard the International Space Station as analogs for future robots that will be developed for the Gateway.
+
 The ISAAC project actively develops in a variety of repos hosted at
-Ames and JSC, as described on the (collaboration tools wiki
-page)[https://babelfish.arc.nasa.gov/confluence/display/astrosoft/Astrosoft+Collaboration+Tools]
+Ames and JSC.
 
 This `isaac` repo serves as a master for integrating an end-to-end
-demo that draws on code from the other repos (as well as directly
+demo that draws on code from the other repos as well as directly
 including a significant amount of the ISAAC code, mostly relating to
-the Astrobee robot).
+the Astrobee robot.
+
+
+### System requirements
+
+The `isaac` repo depends on the `astrobee` repo, therefore it inherits
+the same system requirements. You must use Ubuntu 16.04 to 20.04 64-bit. When
+running in simulation, certain Gazebo plugins require appropriate
+graphics drivers.
+
+### Usage
 
 There are two main ways to install and run `isaac`:
 
@@ -25,22 +37,13 @@ There are two main ways to install and run `isaac`:
    in your native OS, and you don't need to install for development
    prior to installing for demo.
 
-### System requirements
-
-The `isaac` repo depends on the `astrobee` repo, therefore it inherits
-the same system requirements. You must use Ubuntu 16.04 to 20.04 64-bit. When
-running in simulation, certain Gazebo plugins require appropriate
-graphics drivers. See INSTALL.md in that repository for more
-information.
-
-### Usage
-
-[Instructions on installing and using the ISAAC Software](INSTALL.md). [For running
-the demos](DEMO_INSTALL.md)
+[Instructions on installing and using the ISAAC Software](https://nasa.github.io/isaac/html/md_INSTALL.html). For running the [docker demos](https://nasa.github.io/isaac/html/md_DEMO_INSTALL.html)
 
 ### Documentation
 
-There are Doxygen comments in the header files. To compile (make sure you have the latest doxygen installed):
+[The documentation is auto-generated from the contents of this repository.](https://nasa.github.io/isaac/documentation.html)
+
+To compile the documentation locally (make sure you have the latest doxygen installed):
 
     doxygen isaac.doxyfile
 
@@ -48,8 +51,8 @@ There are Doxygen comments in the header files. To compile (make sure you have t
 
 The ISAAC Software is open source, and we welcome contributions from the public.
 Please submit pull requests to the `develop` branch. For us to merge any pull
-requests, we must request that contributors sign and submit a Contributor License
-Agreement due to NASA legal requirements. Thank you for your understanding.
+requests, we must request that contributors sign and submit either an [Individual Contributor License Agreement](https://github.com/nasa/isaac/blob/94996bc1a20fa090336e67b3db5c10a9bb30f0f7/doc/cla/ISAAC_Individual%20CLA.pdf) or a [Corporate Contributor License
+Agreement](https://github.com/nasa/isaac/blob/94996bc1a20fa090336e67b3db5c10a9bb30f0f7/doc/cla/ISAAC_Corporate%20CLA.pdf) due to NASA legal requirements. Thank you for your understanding.
 
 ### License
 
--- dense_map/geometry_mapper/tools/cameras_to_texrecon.py
+++ dense_map/geometry_mapper/tools/cameras_to_texrecon.py
@@ -18,79 +18,101 @@
 
 # Create camera files that texrecon will understand.
 
-import sys, os, re, glob, argparse
+import argparse
+import glob
+import os
+import re
+import sys
+
 import numpy as np
 
-parser = argparse.ArgumentParser(description='Convert cameras to the format of texrecon.')
-parser.add_argument('--camera_dir', default = '', help='The directory containing the camera information (the output of geometry_mapper).')
-parser.add_argument('--undistorted_image_dir', default = '', help='The directory containing the undistorted images.')
-parser.add_argument('--camera_type', default = '', help='The camera type (nav_cam, haz_cam, or sci_cam).')
+parser = argparse.ArgumentParser(
+    description="Convert cameras to the format of texrecon."
+)
+parser.add_argument(
+    "--camera_dir",
+    default="",
+    help="The directory containing the camera information (the output of geometry_mapper).",
+)
+parser.add_argument(
+    "--undistorted_image_dir",
+    default="",
+    help="The directory containing the undistorted images.",
+)
+parser.add_argument(
+    "--camera_type", default="", help="The camera type (nav_cam, haz_cam, or sci_cam)."
+)
 
 args = parser.parse_args()
 
 if args.camera_dir == "" or args.undistorted_image_dir == "" or args.camera_type == "":
-    print("Must specify the camera directory, directory of undistorted images, and camera type.")
+    print(
+        "Must specify the camera directory, directory of undistorted images, and camera type."
+    )
     sys.exit(1)
 
-if args.camera_type != "nav_cam"  and args.camera_type != "haz_cam" \
-       and args.camera_type != "sci_cam":
+if (
+    args.camera_type != "nav_cam"
+    and args.camera_type != "haz_cam"
+    and args.camera_type != "sci_cam"
+):
     print("The camera type must be nav_cam, haz_cam, or sci_cam")
     sys.exit(1)
-    
+
 # Read the intrinsics
 intr_file = args.undistorted_image_dir + "/undistorted_intrinsics.txt"
 if not os.path.exists(intr_file):
     print("Missing file: " + intr_file)
     sys.exit(1)
-with open(intr_file, 'r') as f:
+with open(intr_file, "r") as f:
     for line in f:
-        if re.match('^\s*\#', line):
-            continue # ignore the comments
+        if re.match("^\s*\#", line):
+            continue  # ignore the comments
         vals = line.split()
         if len(vals) < 5:
             print("Expecting 5 parameters in " + intr_file)
             sys.exit(1)
         widx = float(vals[0])
         widy = float(vals[1])
-        f    = float(vals[2])
-        cx   = float(vals[3])
-        cy   = float(vals[4])
+        f = float(vals[2])
+        cx = float(vals[3])
+        cy = float(vals[4])
 
         max_wid = widx
-        if (widy > max_wid):
+        if widy > max_wid:
             max_wid = widy
- 
+
         # normalize
-        nf  = f / max_wid
+        nf = f / max_wid
         ncx = cx / widx
         ncy = cy / widy
-        d0  = 0.0
-        d1  = 0.0
+        d0 = 0.0
+        d1 = 0.0
         paspect = 1.0
-        break # finished reading the line we care for
-    
+        break  # finished reading the line we care for
+
 # Convert the cameras to texrecon's format
 suffix = "_" + args.camera_type + "_to_world.txt"
 
 # Get the cameras to write based on the list of images in the index
 # We avoid simply doing an ls in that directory to ensure we don't
 # run into old files
-index_file = os.path.join(args.camera_dir, args.camera_type + '_index.txt')
+index_file = os.path.join(args.camera_dir, args.camera_type + "_index.txt")
 
 with open(index_file, "r") as f:
     for image_file in f:
         image_file = image_file.rstrip()
         image_file = os.path.basename(image_file)
-        
-        m = re.match('^(.*?)\.jpg', image_file)
+
+        m = re.match("^(.*?)\.jpg", image_file)
         if not m:
             print("Expecting a .jpg file, but got: " + image_file)
 
         in_cam = os.path.join(args.camera_dir, m.group(1) + suffix)
-        
+
         out_cam = args.undistorted_image_dir + "/" + os.path.basename(in_cam)
 
-        m = re.match('^(.*?)' + suffix, out_cam)
+        m = re.match("^(.*?)" + suffix, out_cam)
         if not m:
             print("Could not match desired expression.")
             sys.exit(1)
@@ -99,26 +121,34 @@ with open(index_file, "r") as f:
         if not os.path.exists(in_cam):
             print("Cannot find: " + in_cam)
             sys.exit(1)
-            
-        M = np.loadtxt(in_cam) # camera to world
-        M = np.linalg.inv(M)   # world to camera
+
+        M = np.loadtxt(in_cam)  # camera to world
+        M = np.linalg.inv(M)  # world to camera
 
         print("Writing: " + out_cam)
-        with open(out_cam, 'w') as f:
+        with open(out_cam, "w") as f:
 
             # translation
-            f.write("%0.17g %0.17g %0.17g " % \
-                    (M[0][3], M[1][3], M[2][3]))
+            f.write("%0.17g %0.17g %0.17g " % (M[0][3], M[1][3], M[2][3]))
 
             # rotation
-            f.write("%0.17g %0.17g %0.17g %0.17g %0.17g %0.17g %0.17g %0.17g %0.17g\n" % \
-                    (M[0][0], M[0][1], M[0][2],
-                     M[1][0], M[1][1], M[1][2],
-                     M[2][0], M[2][1], M[2][2]))
+            f.write(
+                "%0.17g %0.17g %0.17g %0.17g %0.17g %0.17g %0.17g %0.17g %0.17g\n"
+                % (
+                    M[0][0],
+                    M[0][1],
+                    M[0][2],
+                    M[1][0],
+                    M[1][1],
+                    M[1][2],
+                    M[2][0],
+                    M[2][1],
+                    M[2][2],
+                )
+            )
 
             # normaized inrinsics
-            f.write("%0.17g %0.17g %0.17g %0.17g %0.17g %0.17g\n" % \
-                    (nf, d0, d1, paspect, ncx, ncy))
-
-
-
+            f.write(
+                "%0.17g %0.17g %0.17g %0.17g %0.17g %0.17g\n"
+                % (nf, d0, d1, paspect, ncx, ncy)
+            )
