bef7b9c2bb39ce5b991df29f90fa926fa0a3d2e4
Trey Smith <trey.smith@nasa.gov>
2022-02-11 16:05:14 -0800

    pano_orientations.py: reduce run time by requiring fewer images per row at high tilt

--- astrobee/behaviors/inspection/scripts/pano_orientations.py
+++ astrobee/behaviors/inspection/scripts/pano_orientations.py
@@ -3,12 +3,12 @@
 """
 This is a reference implementation for how to generate panorama orientations given a
 configuration that includes the desired pan and tilt ranges, camera field of view,
-desired overlap between consecutive images, and a pre-tilt parameter that helps
-with efficiently capturing directional panoramas near tilt = +/- 90.
+desired overlap between consecutive images, and attitude tolerance to account
+for the possibility of pointing error.
 
 The main function of interest is panoOrientations(). There is also some
 validation logic, and two of the test cases are candidates for the configurations to
-test during the SoundSee-Data-3 ISS activity.
+test during ISAAC ISS activities.
 """
 
 import math
@@ -16,18 +16,35 @@ import math
 import numpy as np
 
 EPS = 1e-5
-ROLL, PITCH, YAW = 0, 1, 2
+
+
+def getHFovEffective(hFov, vFov, tilt):
+    """
+    Returns the effective HFOV for an image with tilt centered at @tilt.
+    At large tilt values ("high latitude"), effective HFOV is larger
+    because the parallels are shorter near the pole. We conservatively
+    take the effective HFOV from either the middle, top, or bottom edge
+    of the image, whichever is smallest.
+
+    :param float hFov: Horizontal field of view of each image (degrees).
+    :param float vFov: Vertical field of view of each image (degrees).
+    :param float tilt: The tilt of the image center (degrees).
+    :return: The effective HFOV (degrees).
+    """
+    vRadius = 0.5 * vFov
+    minTheta = min(abs(tilt - vRadius), abs(tilt), abs(tilt + vRadius))
+    return hFov / math.cos(minTheta * math.pi / 180)
 
 
 def pano1D(rangeRadius, fov, overlap, attitudeTolerance):
     """
     Returns image center coordinates such that images cover the range
-    -@rangeMin .. +@rangeRadius with at least the specified @overlap. If one
-    image suffices, it will be centered between @rangeMin and @rangeMax (and
-    the edges of the image will beyond the range if it is smaller than @fov).
-    If more than one image is needed to cover the range, the boundary images
-    will cover exactly to the edges of the specified range and the images
-    will be evenly spaced.
+    -@rangeRadius .. +@rangeRadius with at least the specified
+    @overlap. If one image suffices, it will be centered at 0 (and the
+    edges of the image will extend beyond the range if it is smaller
+    than @fov).  If more than one image is needed to cover the range,
+    the boundary images will cover exactly to the edges of the specified
+    range (modulo @attitudeTolerance) and the images will be evenly spaced.
 
     :param float rangeRadius: Images must cover -rangeRadius .. +rangeRadius (degrees).
     :param float fov: Field of view of each image (degrees).
@@ -48,13 +65,21 @@ def pano1D(rangeRadius, fov, overlap, attitudeTolerance):
     # k - 1 >= (W + 2 * attitudeTolerance - fov) / (fov * (1 - overlap) - attitudeTolerance)
     # k >= (W + 2 * attitudeTolerance - fov) / (fov * (1 - overlap) - attitudeTolerance) + 1
 
-    k = math.ceil((W + 2 * attitudeTolerance - fov) / (fov * (1 - overlap) - attitudeTolerance)) + 1
+    k = (
+        math.ceil(
+            (W + 2 * attitudeTolerance - fov)
+            / (fov * (1 - overlap) - attitudeTolerance)
+        )
+        + 1
+    )
 
     if 1:
         # optional sanity checks
 
         stride = (W + 2 * attitudeTolerance - fov) / (k - 1)
-        assertLte(stride, fov * (1 - overlap) - attitudeTolerance, EPS)  # sufficient overlap
+        assertLte(
+            stride, fov * (1 - overlap) - attitudeTolerance, EPS
+        )  # sufficient overlap
 
         # check if we have more images than necessary
         if k == 1:
@@ -63,7 +88,9 @@ def pano1D(rangeRadius, fov, overlap, attitudeTolerance):
             assertLte(fov, W + 2 * attitudeTolerance, EPS)  # k = 1 is not enough
         else:
             stride1 = (W + 2 * attitudeTolerance - fov) / (k - 2)
-            assertGte(stride1, fov * (1 - overlap) - attitudeTolerance, EPS)  # k is minimized
+            assertGte(
+                stride1, fov * (1 - overlap) - attitudeTolerance, EPS
+            )  # k is minimized
 
     minCenter = -(rangeRadius + attitudeTolerance) + fov / 2
     maxCenter = -minCenter
@@ -79,13 +106,36 @@ def pano1DCompletePan(fov, overlap, attitudeTolerance):
     :param float fov: Field of view of each image (degrees).
     :param float overlap: Minimum required overlap between consecutive images, as a proportion of the image field of view (0 .. 1).
     :param float attitudeTolerance: Ensure overlap criterion is met even if relative attitude between a pair of adjacent images, or between an image and the desired pano boundary, is off by at most this much (degrees).
-    :return: A vector of orientations of image centers.
+    :return: A vector of orientations of image centers (degrees).
     """
     k = math.ceil(360 / (fov * (1 - overlap) - attitudeTolerance))
     centers = np.linspace(-180, 180, num=k, endpoint=False)
     # ensure pano is centered at pan = 0
     midPoint = np.mean((centers[0], centers[-1]))
     return centers - midPoint
+
+
+def pano1DPan(panRadius, hFov, vFov, overlap, attitudeTolerance, tilt):
+    """
+    Returns image center coordinates for a row of images at tilt value
+    @tilt that cover the range -@panRadius .. +@panRadius with at least
+    the specified @overlap. Special complete wrap-around behavior is
+    triggered when @panRadius is exactly 180.
+
+    :param float panRadius: Cover pan angle of -panRadius to +panRadius (degrees).
+    :param float hFov: Horizontal field of view of each image (degrees).
+    :param float vFov: Vertical field of view of each image (degrees).
+    :param float overlap: Minimum required overlap between consecutive images, as a proportion of the image field of view (0 .. 1).
+    :param float attitudeTolerance: Ensure overlap criterion is met even if relative attitude between a pair of adjacent images is off by at most this much (degrees).
+    :param float tilt: The tilt of the image center (degrees).
+    :return: A vector of pan orientations of image centers (degrees).
+    """
+    hFovEffective = getHFovEffective(hFov, vFov, tilt)
+    # print("tilt=%s hFovEffective=%s" % (tilt, hFovEffective))
+    if panRadius == 180:
+        return pano1DCompletePan(hFovEffective, overlap, attitudeTolerance)
+    else:
+        return pano1D(panRadius, hFovEffective, overlap, attitudeTolerance)
 
 
 def panoOrientations(panRadius, tiltRadius, hFov, vFov, overlap, attitudeTolerance):
@@ -103,32 +153,78 @@ def panoOrientations(panRadius, tiltRadius, hFov, vFov, overlap, attitudeToleran
     :return: (imageCenters, ncols, nrows). A list of orientations of image centers, the number of columns in the panorama, and the number of rows.
     """
 
-    if panRadius == 180:
-        panVals = pano1DCompletePan(hFov, overlap, attitudeTolerance)
-    else:
-        panVals = pano1D(panRadius, hFov, overlap, attitudeTolerance)
-    tiltVals = pano1D(tiltRadius, vFov, overlap, attitudeTolerance)
-
-    # Images are in column-major order. When capturing a long panorama with
-    # people present, this makes it easier for them to move out of the field of
-    # view as needed. Order columns left to right, and within each column, tilt
-    # top to bottom. That is just more intuitive for people's expectations.
+    # calculate all image centers
     imageCenters = []
-    for pan in panVals:
-        for tilt in reversed(tiltVals):
-            imageCenters.append(np.array((pan, tilt)))
-    return (imageCenters, len(panVals), len(tiltVals))
+    tiltVals = pano1D(tiltRadius, vFov, overlap, attitudeTolerance)
+    for iy, tilt in enumerate(reversed(tiltVals)):
+        panVals = pano1DPan(panRadius, hFov, vFov, overlap, attitudeTolerance, tilt)
+        for pan in panVals:
+            imageCenters.append((pan, tilt, iy, -1))
+    # imageCenters = np.array(imageCenters)
+    # print(imageCenters.shape)
+    imageCenters = np.array(
+        imageCenters,
+        dtype=[
+            ("pan", np.double),
+            ("tilt", np.double),
+            ("iy", np.int16),
+            ("ix", np.int16),
+        ],
+    )
+
+    # assign image centers to columns
+    minTilt = np.min(np.abs(tiltVals))
+    columnCenters = pano1DPan(
+        panRadius, hFov, vFov, overlap, attitudeTolerance, minTilt
+    )
+    imagePans = imageCenters["pan"]
+    imageCenters["ix"] = np.array(
+        [np.argmin(np.abs(columnCenters - p)) for p in imagePans]
+    )
+
+    # order images in column-major order; alternate direction top-to-bottom or bottom-to-top
+    imagesOrdered = []
+    for ix, pan in enumerate(columnCenters):
+        imagesInColInd = imageCenters["ix"] == ix
+        imagesInCol = imageCenters[imagesInColInd]
+
+        # sort on tilt bottom-to-top
+        imagesInCol = imagesInCol[np.argsort(imagesInCol["tilt"])]
+
+        # on even-numbered columns, reverse tilt order, top-to-bottom
+        if ix % 2 == 0:
+            imagesInCol = imagesInCol[::-1]
+
+        imagesOrdered += list(imagesInCol)
+    imagesOrdered = np.array(imagesOrdered)
+
+    ncols = len(columnCenters)
+    nrows = len(tiltVals)
+
+    return imagesOrdered, ncols, nrows
 
 
 def printPano(pano):
     imageCenters, ncols, nrows = pano
-    print("%s cols x %s rows [pan tilt]" % (ncols, nrows))
-    for i in range(nrows):
+    numImages = imageCenters.shape[0]
+    print(
+        "%s images, %s cols x %s rows, frame# [pan tilt]:" % (numImages, ncols, nrows)
+    )
+    if 0:
+        print(imageCenters)
+        return
+    imageLookup = {
+        (iy, ix): (i, pan, tilt) for i, (pan, tilt, iy, ix) in enumerate(imageCenters)
+    }
+    for iy in range(nrows):
         print("  ", end="")
-        for j in range(ncols):
-            # print(i, j, j * nrows + i)
-            imageCenter = imageCenters[j * nrows + i]
-            print(np.around(imageCenter), end=" ")
+        for ix in range(ncols):
+            imageInfo = imageLookup.get((iy, ix))
+            if imageInfo is None:
+                print("              ", end="   ")
+            else:
+                i, pan, tilt = imageInfo
+                print("%2d [%4d %4d]" % (i, round(pan), round(tilt)), end="   ")
         print()
 
 
@@ -148,84 +244,11 @@ def assertGte(a, b, eps):
     assert a >= b + eps, "FAIL: %s should be >= %s, within tolerance %s" % (a, b, eps)
 
 
-def checkPano(pano, panRadius, tiltRadius, hFov, vFov, overlap, attitudeTolerance):
-    return  # temp
-    imageCenters, ncols, nrows = pano
-
-    topLeft = getEuler(imageCenters[0])
-
-    if ncols == 1:
-        assertLte(panMax - panMin, hFov, EPS)  # one column is enough
-    else:
-        nextPan = getEuler(imageCenters[nrows])
-        panStride = nextPan[YAW] - topLeft[YAW]
-        assertLte(panStride, hFov * (1 - overlap), EPS)  # pan overlaps enough
-
-    if nrows == 1:
-        assertLte(tiltMax - tiltMin, vFov, EPS)  # one row is enough
-    else:
-        nextTilt = getEuler(imageCenters[1])
-        tiltStride = -(nextTilt[PITCH] - topLeft[PITCH])
-        assertLte(tiltStride, vFov * (1 - overlap), EPS)  # tilt overlaps enough
-
-    # we shouldn't be able to remove a column
-    if ncols == 1:
-        pass  # obviously can't remove single column
-    elif ncols == 2:
-        if panMin == -180 and panMax == 180:
-            assertLte(hFov * (1 - overlap), 360)  # one column is not enough
-        else:
-            assertLte(hFov, panMax - panMin, EPS)  # one column is not enough
-    else:
-        if panMin == -180 and panMax == 180:
-            panStride1 = 360 / (ncols - 1)
-        else:
-            panStride1 = ((panMax - panMin) - hFov) / (ncols - 2)
-        assertGte(panStride1, hFov * (1 - overlap), EPS)  # ncols is minimized
-
-    # we shouldn't be able to remove a row
-    if nrows == 1:
-        pass  # obviously can't remove single row
-    elif nrows == 2:
-        assertLte(vFov, tiltMax - tiltMin, EPS)  # one row is not enough
-    else:
-        tiltStride1 = ((tiltMax - tiltMin) - vFov) / (nrows - 2)
-        assertGte(tiltStride1, vFov * (1 - overlap), EPS)  # nrows is minimized
-
-    bottomRight = getEuler(imageCenters[-1])
-
-    panCenterMin = topLeft[YAW]
-    panCenterMax = bottomRight[YAW]
-    if panMin == -180 and panMax == 180:
-        assertEqual(panStride * ncols, 360, EPS)  # evenly spaced
-        midPan = np.mean((panCenterMin, panCenterMax))
-        assertEqual(midPan, 0, EPS)  # centered
-    else:
-        if ncols == 1:
-            assertEqual(
-                panCenterMin, 0.5 * (panMin + panMax), EPS
-            )  # single column is centered
-        else:
-            assertEqual(panCenterMin - hFov / 2, panMin, EPS)  # covers to panMin
-            assertEqual(panCenterMax + hFov / 2, panMax, EPS)  # covers to panMax
-
-    tiltCenterMax = topLeft[PITCH]
-    tiltCenterMin = bottomRight[PITCH]
-
-    if nrows == 1:
-        assertEqual(
-            tiltCenterMin, 0.5 * (tiltMin + tiltMax), EPS
-        )  # single row is centered
-    else:
-        assertEqual(tiltCenterMin - vFov / 2, tiltMin, EPS)  # covers to tiltMin
-        assertEqual(tiltCenterMax + vFov / 2, tiltMax, EPS)  # covers to tiltMax
-
-
 def testCase(label, config):
     print(label, end=": ")
     pano = panoOrientations(**config)
     printPano(pano)
-    checkPano(pano, **config)
+    print()
 
 
 # Based on field_of_view_calculator.py, consulting with Oleg, we have:
