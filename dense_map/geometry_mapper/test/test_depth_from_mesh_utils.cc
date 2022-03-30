2ea6d562e35f1fb1aab1c7e14a7609b95b237e43
Ryan Soussan <ryan.soussan@gmail.com>
2022-03-30 15:46:32 -0700

    updated test

--- dense_map/geometry_mapper/test/test_depth_from_mesh_utils.cc
+++ dense_map/geometry_mapper/test/test_depth_from_mesh_utils.cc
@@ -17,12 +17,62 @@
  * under the License.
  */
 
+#include <depth_from_mesh_utils.h>
+#include <texture_processing.h>
+
 #include <gtest/gtest.h>
 
+namespace dm = dense_map;
 
-TEST(DepthOdometryWrapperTester, PointToPlaneICP) {
+TEST(DepthFromMeshTester, NoRotationDepths) {
+  std::vector<unsigned int> ids{0, 1, 2};
+  std::vector<math::Vec3f> vertices;
+  // Build a mesh with a single triangule along the yz plane, offset from the x axis by 1
+  vertices.emplace_back(dm::eigen_to_vec3f(Eigen::Vector3d(1, -1, 0)));
+  vertices.emplace_back(dm::eigen_to_vec3f(Eigen::Vector3d(1, 1, 0)));
+  vertices.emplace_back(dm::eigen_to_vec3f(Eigen::Vector3d(1, 1, 1)));
+  const auto bvh_tree = BVHTree::create(ids, vertices);
+  // Poses with ray facing mesh
+  {
+    const auto world_T_sensor = Eigen::Isometry3d::Identity();
+    // Create a ray facing along the x axis
+    const Eigen::Vector3d sensor_t_ray(1, 0, 0);
+    const auto depth = dm::Depth(sensor_t_ray, world_T_sensor, *bvh_tree);
+    ASSERT_TRUE(depth != boost::none);
+    EXPECT_NEAR(*depth, 1, 1e-6);
+  }
+  {
+    auto world_T_sensor = Eigen::Isometry3d::Identity();
+    world_T_sensor.translation().x() = -1;
+    // Create a ray facing along the x axis
+    const Eigen::Vector3d sensor_t_ray(1, 0, 0);
+    const auto depth = dm::Depth(sensor_t_ray, world_T_sensor, *bvh_tree);
+    ASSERT_TRUE(depth != boost::none);
+    EXPECT_NEAR(*depth, 2, 1e-6);
+  }
+  // Poses with ray facing away from mesh
+  {
+    const auto world_T_sensor = Eigen::Isometry3d::Identity();
+    // Create a ray facing along the negative x axis
+    const Eigen::Vector3d sensor_t_ray(-1, 0, 0);
+    const auto depth = dm::Depth(sensor_t_ray, world_T_sensor, *bvh_tree);
+    EXPECT_TRUE(depth == boost::none);
+  }
+  {
+    const auto world_T_sensor = Eigen::Isometry3d::Identity();
+    // Create a ray facing along the y axis
+    const Eigen::Vector3d sensor_t_ray(0, 1, 0);
+    const auto depth = dm::Depth(sensor_t_ray, world_T_sensor, *bvh_tree);
+    EXPECT_TRUE(depth == boost::none);
+  }
+  {
+    const auto world_T_sensor = Eigen::Isometry3d::Identity();
+    // Create a ray facing along the z axis
+    const Eigen::Vector3d sensor_t_ray(0, 0, 1);
+    const auto depth = dm::Depth(sensor_t_ray, world_T_sensor, *bvh_tree);
+    EXPECT_TRUE(depth == boost::none);
+  }
 }
-
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char** argv) {
