c572da2cdc3a214a23148ef1bc4ab3c9d0b1b512
Trey Smith <trey.smith@nasa.gov>
2023-02-06 10:37:36 -0800

    basic local storage working

--- pano/pano_view/static/js/isaac_source_image.js
+++ pano/pano_view/static/js/isaac_source_image.js
@@ -53,9 +53,98 @@ function parseUrlParameters() {
     return configFromUrl;
 }
 
+function isaacSetDefault(obj, field, defaultValue) {
+    if (obj[field] == undefined) {
+	obj[field] = defaultValue;
+	return defaultValue;
+    }
+    return obj[field];
+}
+
+function isaacStorageGetRoot() {
+    var storageItemText = window.localStorage.getItem('annotations') || "{}";
+    return JSON.parse(storageItemText);
+}
+
+function isaacStorageSetRoot(obj) {
+    window.localStorage.setItem('annotations', JSON.stringify(obj));
+}
+
+function isaacStorageGet(fieldPath) {
+    var storageItem = isaacStorageGetRoot();
+
+    var obj = storageItem;
+    for (fieldElt of fieldPath) {
+	obj = obj[fieldElt];
+	if (obj == undefined) {
+	    return undefined;
+	}
+    }
+    return obj;
+}
+
+function isaacStorageSet(fieldPath, value) {
+    var storageItem = isaacStorageGetRoot();
+
+    var obj = storageItem;
+    for (fieldElt of fieldPath.slice(0, -1)) {
+	obj = isaacSetDefault(obj, fieldElt, {});
+    }
+    obj[fieldPath[fieldPath.length - 1]] = value;
+
+    isaacStorageSetRoot(storageItem);
+}
+
+function isaacStorageSetDefault(fieldPath, defaultValue) {
+    var storageItem = isaacStorageGetRoot();
+
+    var obj = storageItem;
+    for (fieldElt of fieldPath.slice(0, -1)) {
+	obj = isaacSetDefault(obj, fieldElt, {});
+    }
+
+    var fieldLast = fieldPath[fieldPath.length - 1];
+    if (obj[fieldLast] == undefined) {
+	obj[fieldLast] = defaultValue;
+	isaacStorageSetRoot(storageItem);
+	return defaultValue;
+    }
+    return obj[fieldLast];
+}
+
+function isaacStorageDelete(fieldPath) {
+    var storageItem = isaacStorageGetRoot();
+
+    var obj = storageItem;
+    for (fieldElt of fieldPath.slice(0, -1)) {
+	obj = isaacSetDefault(obj, fieldElt, {});
+    }
+    delete obj[fieldPath[fieldPath.length - 1]];
+
+    isaacStorageSetRoot(storageItem);
+}
+
+function isaacSaveData(data, fileName) {
+    var json = JSON.stringify(data, null, 4);
+    var blob = new Blob([json], {type: 'application/json'});
+    var url = window.URL.createObjectURL(blob);
+
+    var a = document.createElement("a");
+    a.href = url;
+    a.download = fileName;
+    a.style = "display: none";
+    document.body.appendChild(a);
+
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+    a.remove();
+}
+
 function initIsaacSourceImage() {
     var configFromUrl = parseUrlParameters();
 
+    // Initialize OpenSeaDragon viewer and Annotorious plugin
     var viewer = OpenSeadragon({
         id: 'container',
         prefixUrl: '../../media/openseadragon/',
@@ -64,12 +153,62 @@ function initIsaacSourceImage() {
     });
     var anno = OpenSeadragon.Annotorious(viewer);
 
+    // Export symbols for debugging
+    window.configFromUrl = configFromUrl;
+    window.viewer = viewer;
+    window.anno = anno;
+
+    // Configure extra point drawing tool (default tools are rect and polygon only)
     Annotorious.SelectorPack(anno, {
 	tools: ['point']
     });
 
-    console.log(document.getElementById('isaac-toolbar-container'));
+    // Create toolbar
     Annotorious.Toolbar(anno, document.getElementById('isaac-toolbar-container'));
+
+    // Configure other button handlers
+    document.getElementById('isaac-save').addEventListener("click", function(event) {
+	isaacSaveData(isaacStorageGetRoot(), 'annotations.json');
+    });
+    document.getElementById('isaac-clear').addEventListener("click", function(event) {
+	isaacStorageSetRoot({});
+	anno.clearAnnotations();
+    });
+
+    // Restore annotations on this image from HTML5 local storage
+    var imageAnnotations = isaacStorageGet([configFromUrl.scene, configFromUrl.imageId]);
+    if (imageAnnotations) {
+	anno.setAnnotations(imageAnnotations);
+    }
+
+    // Save subsequent drawing events to HTML5 local storage
+    var imageStoragePath = [configFromUrl.scene, configFromUrl.imageId];
+    anno.on('createAnnotation', function(annotation) {
+	// isaacStorageSet([configFromUrl.scene, configFromUrl.imageId, annotation.id], annotation);
+	var annotations = isaacStorageSetDefault(imageStoragePath, []);
+	annotations.push(annotation);
+	isaacStorageSet(imageStoragePath, annotations);
+    });
+    anno.on('updateAnnotation', function(annotation) {
+	var annotations = isaacStorageSetDefault(imageStoragePath, []);
+
+	var idx = annotations.findIndex(a => (a.id == annotation.id));
+	if (idx == undef) {
+	    console.log('warning: updated element not found, creating instead');
+	    annotations.push(annotation);
+	} else {
+	    annotations[idx] = annotation;
+	}
+
+	isaacStorageSet(imageStoragePath, annotations);
+    });
+    anno.on('deleteAnnotation', function(annotation) {
+	var annotations = isaacStorageSetDefault(imageStoragePath, []);
+
+	annotations = annotations.filter(a => (a.id != annotation.id));
+
+	isaacStorageSet(imageStoragePath, annotations);
+    });
 }
 
 initIsaacSourceImage();
