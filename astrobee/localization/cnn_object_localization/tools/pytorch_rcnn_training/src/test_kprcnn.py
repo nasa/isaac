[FIX] Half precision isn't supported. Reverting.

astrobee/localization/cnn_object_localization/tools/pytorch_rcnn_training/src/test_kprcnn.py
@@ -79,7 +79,7 @@ def get_trained_model(weights_path, num_classes=5):
 
 def main(dataset_path: str, output_path: str, weights_path: str, nms_thresh: Optional[str]):
 
-    model = get_trained_model(weights_path, num_classes=3)
+    model = get_trained_model(weights_path, num_classes=2)
     model.eval()
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")

astrobee/localization/cnn_object_localization/tools/pytorch_rcnn_training/src/train_kprcnn.py
@@ -78,7 +78,7 @@ def main(
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
     # our dataset has two classes only - background and keypoint
-    num_classes = 3
+    num_classes = 2
     # use our dataset and defined transformations
     label_dict = {9: 1, 10: 1}  # two keypoint instances per image, both of the same class
     dataset = AstrobeeHandrailDataset(dataset_path, get_transform(train=True), label_dict=label_dict, images_dir="images", masks_dir="keypoint_masks")