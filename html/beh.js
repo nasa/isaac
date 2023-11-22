eb167e78e20c86a6ba3a09d2f7ad02bd0c37d2f9
Marina Moreira <67443181+marinagmoreira@users.noreply.github.com>
2023-11-22 01:41:28 +0000

    Automatic update for 929261634648190e566fc3b0523fc7e6c2483795.

--- html/beh.js
+++ html/beh.js
@@ -2,6 +2,7 @@ var beh =
 [
     [ "Inspection Behavior", "inspection.html", "inspection" ],
     [ "Cargo Behavior", "cargo.html", [
-      [ "Using the cargo tool", "cargo.html#autotoc_md56", null ]
+      [ "Using the cargo tool", "cargo.html#autotoc_md56", null ],
+      [ "Demo example", "cargo.html#autotoc_md57", null ]
     ] ]
 ];
--- html/idm.js
+++ html/idm.js
@@ -10,55 +10,55 @@ var idm =
       [ "Trigger wifi mapping", "docker.html#autotoc_md21", null ]
     ] ],
     [ "Geometry mapper and streaming mapper", "geometric_streaming_mapper.html", [
-      [ "Running the geometry mapper", "geometric_streaming_mapper.html#autotoc_md96", [
-        [ "Overview", "geometric_streaming_mapper.html#autotoc_md77", null ],
-        [ "Setting up the environment", "geometric_streaming_mapper.html#autotoc_md78", null ],
-        [ "Robot sensors", "geometric_streaming_mapper.html#autotoc_md79", null ],
-        [ "Actual vs simulated robot data", "geometric_streaming_mapper.html#autotoc_md80", null ],
-        [ "Sensors present in simulation", "geometric_streaming_mapper.html#autotoc_md81", null ],
-        [ "Adding support for new cameras", "geometric_streaming_mapper.html#autotoc_md82", null ],
-        [ "Compiling the software", "geometric_streaming_mapper.html#autotoc_md83", [
-          [ "Compiling VoxBlox", "geometric_streaming_mapper.html#autotoc_md84", null ],
-          [ "Compiling CGAL", "geometric_streaming_mapper.html#autotoc_md85", null ],
-          [ "CGAL license", "geometric_streaming_mapper.html#autotoc_md86", null ],
-          [ "Functionality provided by CGAL", "geometric_streaming_mapper.html#autotoc_md87", null ]
+      [ "Running the geometry mapper", "geometric_streaming_mapper.html#autotoc_md97", [
+        [ "Overview", "geometric_streaming_mapper.html#autotoc_md78", null ],
+        [ "Setting up the environment", "geometric_streaming_mapper.html#autotoc_md79", null ],
+        [ "Robot sensors", "geometric_streaming_mapper.html#autotoc_md80", null ],
+        [ "Actual vs simulated robot data", "geometric_streaming_mapper.html#autotoc_md81", null ],
+        [ "Sensors present in simulation", "geometric_streaming_mapper.html#autotoc_md82", null ],
+        [ "Adding support for new cameras", "geometric_streaming_mapper.html#autotoc_md83", null ],
+        [ "Compiling the software", "geometric_streaming_mapper.html#autotoc_md84", [
+          [ "Compiling VoxBlox", "geometric_streaming_mapper.html#autotoc_md85", null ],
+          [ "Compiling CGAL", "geometric_streaming_mapper.html#autotoc_md86", null ],
+          [ "CGAL license", "geometric_streaming_mapper.html#autotoc_md87", null ],
+          [ "Functionality provided by CGAL", "geometric_streaming_mapper.html#autotoc_md88", null ]
         ] ],
-        [ "Data acquisition", "geometric_streaming_mapper.html#autotoc_md88", [
-          [ "With real data", "geometric_streaming_mapper.html#autotoc_md89", null ],
-          [ "With simulated data", "geometric_streaming_mapper.html#autotoc_md90", [
-            [ "Recording simulated data", "geometric_streaming_mapper.html#autotoc_md91", null ]
+        [ "Data acquisition", "geometric_streaming_mapper.html#autotoc_md89", [
+          [ "With real data", "geometric_streaming_mapper.html#autotoc_md90", null ],
+          [ "With simulated data", "geometric_streaming_mapper.html#autotoc_md91", [
+            [ "Recording simulated data", "geometric_streaming_mapper.html#autotoc_md92", null ]
           ] ]
         ] ],
-        [ "Data pre-processing", "geometric_streaming_mapper.html#autotoc_md92", null ],
-        [ "Camera calibration", "geometric_streaming_mapper.html#autotoc_md93", null ],
-        [ "Data extraction", "geometric_streaming_mapper.html#autotoc_md94", null ],
-        [ "Map building and registration", "geometric_streaming_mapper.html#autotoc_md95", null ],
-        [ "When using real data", "geometric_streaming_mapper.html#autotoc_md97", null ],
-        [ "With simulated data", "geometric_streaming_mapper.html#autotoc_md98", null ],
-        [ "Running the streaming mapper", "geometric_streaming_mapper.html#autotoc_md99", [
-          [ "Running with real data", "geometric_streaming_mapper.html#autotoc_md100", [
-            [ "When the robot (or nav cam) poses are known", "geometric_streaming_mapper.html#autotoc_md101", null ],
-            [ "Running with no robot or nav cam pose information", "geometric_streaming_mapper.html#autotoc_md102", null ],
-            [ "The streaming mapper configuration file", "geometric_streaming_mapper.html#autotoc_md103", null ]
+        [ "Data pre-processing", "geometric_streaming_mapper.html#autotoc_md93", null ],
+        [ "Camera calibration", "geometric_streaming_mapper.html#autotoc_md94", null ],
+        [ "Data extraction", "geometric_streaming_mapper.html#autotoc_md95", null ],
+        [ "Map building and registration", "geometric_streaming_mapper.html#autotoc_md96", null ],
+        [ "When using real data", "geometric_streaming_mapper.html#autotoc_md98", null ],
+        [ "With simulated data", "geometric_streaming_mapper.html#autotoc_md99", null ],
+        [ "Running the streaming mapper", "geometric_streaming_mapper.html#autotoc_md100", [
+          [ "Running with real data", "geometric_streaming_mapper.html#autotoc_md101", [
+            [ "When the robot (or nav cam) poses are known", "geometric_streaming_mapper.html#autotoc_md102", null ],
+            [ "Running with no robot or nav cam pose information", "geometric_streaming_mapper.html#autotoc_md103", null ],
+            [ "The streaming mapper configuration file", "geometric_streaming_mapper.html#autotoc_md104", null ]
           ] ]
         ] ],
-        [ "Running with simulated data", "geometric_streaming_mapper.html#autotoc_md104", null ],
-        [ "Camera refinement", "geometric_streaming_mapper.html#autotoc_md105", [
-          [ "Image selection", "geometric_streaming_mapper.html#autotoc_md106", null ],
-          [ "Map building and registration", "geometric_streaming_mapper.html#autotoc_md107", null ],
-          [ "Running camera_refiner", "geometric_streaming_mapper.html#autotoc_md108", null ],
-          [ "The algorithm", "geometric_streaming_mapper.html#autotoc_md109", null ],
-          [ "Camera refiner options", "geometric_streaming_mapper.html#autotoc_md110", null ],
-          [ "Using the refiner with a radtan model for nav_cam", "geometric_streaming_mapper.html#autotoc_md111", null ]
+        [ "Running with simulated data", "geometric_streaming_mapper.html#autotoc_md105", null ],
+        [ "Camera refinement", "geometric_streaming_mapper.html#autotoc_md106", [
+          [ "Image selection", "geometric_streaming_mapper.html#autotoc_md107", null ],
+          [ "Map building and registration", "geometric_streaming_mapper.html#autotoc_md108", null ],
+          [ "Running camera_refiner", "geometric_streaming_mapper.html#autotoc_md109", null ],
+          [ "The algorithm", "geometric_streaming_mapper.html#autotoc_md110", null ],
+          [ "Camera refiner options", "geometric_streaming_mapper.html#autotoc_md111", null ],
+          [ "Using the refiner with a radtan model for nav_cam", "geometric_streaming_mapper.html#autotoc_md112", null ]
         ] ],
-        [ "Orthoprojecting images", "geometric_streaming_mapper.html#autotoc_md112", null ]
+        [ "Orthoprojecting images", "geometric_streaming_mapper.html#autotoc_md113", null ]
       ] ]
     ] ],
     [ "Volumetric Mapper", "volumetric_mapper.html", [
-      [ "Volumetric Mapper", "volumetric_mapper.html#autotoc_md113", [
-        [ "Overview", "volumetric_mapper.html#autotoc_md114", null ],
-        [ "Trace Map", "volumetric_mapper.html#autotoc_md115", null ],
-        [ "Wifi 3D Map", "volumetric_mapper.html#autotoc_md116", null ]
+      [ "Volumetric Mapper", "volumetric_mapper.html#autotoc_md114", [
+        [ "Overview", "volumetric_mapper.html#autotoc_md115", null ],
+        [ "Trace Map", "volumetric_mapper.html#autotoc_md116", null ],
+        [ "Wifi 3D Map", "volumetric_mapper.html#autotoc_md117", null ]
       ] ]
     ] ]
 ];
