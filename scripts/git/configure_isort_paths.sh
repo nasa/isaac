308c6094c51c2af9bfe041b53f2f7f7e05243b25
Trey Smith <trey.smith@nasa.gov>
2024-01-26 15:20:36 -0500

    Regenerate isort paths. Propagate excluded folders to isort path script.

--- scripts/git/configure_isort_paths.sh
+++ scripts/git/configure_isort_paths.sh
@@ -27,7 +27,15 @@ srcdir=$(cd $thisdir/../.. && pwd)
 cd $srcdir
 
 # Generate a comma-separated list of folders containing *.py files
-pydirs=$(find . -name "*.py" -print0 | xargs -0 dirname | cut -c3- | sort | uniq | paste -sd "," -)
+pydirs=$( \
+    find . -name "*.py" -print0 \
+    | xargs -0 dirname \
+    | cut -c3- \
+    | grep -Pv "^cmake|^submodules|^astrobee/survey_manager/survey_dependencies|^scripts/setup/dependencies" \
+    | sort \
+    | uniq \
+    | paste -sd "," - \
+)
 
 # Overwrite the src_paths line in the config file to use the list
 perl -i -ple "if (/^src_paths = /) { \$_ = 'src_paths = $pydirs'; }" .isort.cfg
